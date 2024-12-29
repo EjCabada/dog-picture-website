6683abd5784ed57514d3939f2e528b5a57917486
EJ Cabada <cabada.eric7@gmail.com>
2024-12-28 21:13:49 -0800

    trying to set up personal pages

--- dog website/src/main.jsx
+++ dog website/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client'; 
+import './index.css';
+import Layout from '../src/Components/Layout.jsx';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import Home from '../src/Components/Home.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} /> 
+    </Route>
+  )
+);
+
+const root = createRoot(document.getElementById('root')); 
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
